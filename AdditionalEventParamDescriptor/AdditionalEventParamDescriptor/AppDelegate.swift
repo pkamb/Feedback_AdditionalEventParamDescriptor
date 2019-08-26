18f40cc3ed0e08f28582b5f61798a7b666adccc7
Peter Kamb <peterkamb@gmail.com>
2019-08-26 14:14:46 -0700

    allow multiple selection

--- AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
+++ AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
@@ -63,6 +63,7 @@ class ViewController: NSViewController {
     
     @IBAction func openFiles(sender: NSButton) {
         let openPanel = NSOpenPanel()
+        openPanel.allowsMultipleSelection = true
         guard openPanel.runModal() != .cancel else {
             return
         }
