0edf78f4328daabd2a7ac8855fbb6cf264d62a9d
Peter Kamb <peterkamb@gmail.com>
2019-08-26 13:16:49 -0700

    add new Xcode project

---
+++ AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  AdditionalEventParamDescriptor
+//
+//  Created by Peter Kamb on 8/26/19.
+//  Copyright © 2019 Feedback. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
+
