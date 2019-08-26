646a6a8414d6f55716108887a75aa75940414523
Peter Kamb <peterkamb@gmail.com>
2019-08-26 13:20:11 -0700

    add event handling

--- AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
+++ AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
@@ -12,11 +12,45 @@ import Cocoa
 class AppDelegate: NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        NSAppleEventManager.shared().setEventHandler(self,
+                                                     andSelector: #selector(handle(event:replyEvent:)),
+                                                     forEventClass: AEEventClass(kCoreEventClass),
+                                                     andEventID: AEEventID(kAEOpenDocuments))
     }
     
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func open(urls: [URL]) {
+        let additionalEvent = NSAppleEventDescriptor(eventClass:       AEEventClass(kCoreEventClass),
+                                                     eventID:          AEEventID(kAEOpenDocuments),
+                                                     targetDescriptor: NSAppleEventDescriptor(bundleIdentifier: Bundle.main.bundleIdentifier!),
+                                                     returnID:         AEReturnID(kAutoGenerateReturnID),
+                                                     transactionID:    AETransactionID(kAnyTransactionID))
+        
+        let directObject = NSAppleEventDescriptor(string: "MY_CUSTOM_APPLE_EVENT")
+        additionalEvent.setDescriptor(directObject, forKeyword: keyDirectObject)
+        
+        NSWorkspace.shared.open(urls,
+                                withAppBundleIdentifier: Bundle.main.bundleIdentifier!,
+                                options: [.withErrorPresentation],
+                                additionalEventParamDescriptor: additionalEvent,
+                                launchIdentifiers: nil)
+    }
+    
+    @objc func handle(event: NSAppleEventDescriptor?, replyEvent: NSAppleEventDescriptor?) {
+        guard let event = event else {
+                return
+        }
+        print(event)
+        
+        guard let additionalEvent = event.paramDescriptor(forKeyword: keyAEPropData) else {
+            return
+        }
+        print(additionalEvent)
+        
+        guard let directObject = additionalEvent.paramDescriptor(forKeyword: keyDirectObject) else {
+            return
+        }
+        print(directObject)
     }
     
 }
