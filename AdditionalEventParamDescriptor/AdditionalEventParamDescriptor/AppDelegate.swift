9b8f957067c454b10af681ca7d92c13e3f86b080
Peter Kamb <peterkamb@gmail.com>
2019-08-26 14:14:36 -0700

    terminate app

--- AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
+++ AdditionalEventParamDescriptor/AdditionalEventParamDescriptor/AppDelegate.swift
@@ -11,6 +11,10 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         
         NSAppleEventManager.shared().setEventHandler(self,
